a8e6db83a4707a385da695a418abc0df00141bb5
LikeJasper <williamclark.home@gmail.com>
2015-12-03 16:28:35 +0100

    Add form submission logic

--- public/javascripts/react-app.js
+++ public/javascripts/react-app.js
@@ -1,16 +1,45 @@
 var LoginForm = React.createClass({
+
+  passwordMinLength: 6,
+  validateEmail: function(email) {},
+  validatePassword: function(password) {},
+
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var inputs = e.target.children;
+    var email = inputs.email.value.trim();
+    var password = inputs.password.value;
+    var validEmail = this.validateEmail(email);
+    var validPassword = this.validatePassword(password);
+
+    if (validEmail && validPassword) {
+      // TODO: Send to server
+      console.log("Sending valid email and password to server...")
+    } else {
+      if (!validEmail) {
+        // TODO: Display email validation help
+        console.log("Displaying email validation help...")
+      }
+      if (!validPassword) {
+        // TODO: Display password validation help
+        console.log("Displaying password validation help...")
+      }
+    }
+  },
+
   render: function() {
     return (
-      <form className="login-form">
+      <form className="login-form" onSubmit={this.handleSubmit}>
         <input
+          name="email"
           type="email"
           placeholder="Your email address"
         />
         <input
+          name="password"
           type="password"
           placeholder="Your password"
-          minlength="6"
-          novalidate="true"
+          minLength={this.passwordMinLength}
         />
         <input
           type="submit"
