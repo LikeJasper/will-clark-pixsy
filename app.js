1e7e3eafde58151b7ed3e6f89e59601e42f2f79f
LikeJasper <williamclark.home@gmail.com>
2015-12-03 15:48:19 +0100

    Simplify and reorganise project structure

--- app.js
+++ app.js
@@ -1,12 +1,7 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
-var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
 
@@ -19,11 +14,11 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.get('/login', function(req, res) {
+  res.render('login');
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
