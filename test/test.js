ff5368c10175edd0141058614cd610cebf1f551a
LikeJasper <williamclark.home@gmail.com>
2015-12-06 18:57:59 +0100

    Add more server test situations

--- test/test.js
+++ test/test.js
@@ -8,8 +8,20 @@ var postLogin = require('../routes/login/postLogin').postLogin;
 var LOGIN_URL = 'http://localhost:3000/login';
 
 var VALID_EMAIL_AND_VALID_PASSWORD = {email: 'ansel@adams.com', password: 'Yosemite'};
-var VALID_EMAIL_AND_INVALID_PASSWORD = {email: 'ansel@adams.com', password: 'punk65'};
-var INVALID_EMAIL = {email: 'ansel@adams.co.uk', password: 'Yosemite'};
+var VALID_EMAIL_AND_INVALID_PASSWORDS = [
+  {email: 'ansel@adams.com', password: ''},
+  {email: 'ansel@adams.com', password: 'yosemite'},
+  {email: 'ansel@adams.com', password: 'YOSEMITE'},
+  {email: 'ansel@adams.com', password: 'punk65'},
+  {email: 'ansel@adams.com'}
+];
+var INVALID_EMAILS = [
+  {email: 'ansel@adams.co.uk', password: 'password'},
+  {email: 'ansel@adams.co.uk', password: ''},
+  {email: 'ansel@adams.co.uk', password: 'Yosemite'},
+  {email: '', password: 'Yosemite'},
+  {password: 'Yosemite'}
+];
 
 var INVALID_EMAIL_RESPONSE = {error: 'Email not found.'};
 var INVALID_PASSWORD_RESPONSE = {error: 'Incorrect password.'};
@@ -55,44 +67,53 @@ describe('Login routes', function () {
     describe('with a valid email and invalid password', function() {
       var invalidPasswordOptions = {
         url: LOGIN_URL,
-        method: 'POST',
-        json: VALID_EMAIL_AND_INVALID_PASSWORD
+        method: 'POST'
       };
 
-      it('should respond with a 401 status code.', function(done) {
-        request(invalidPasswordOptions, function(err, res, body) {
-          expect(res.statusCode).to.equal(401);
-          done();
+      VALID_EMAIL_AND_INVALID_PASSWORDS.forEach(function(credentials) {
+        invalidPasswordOptions.json = credentials;
+
+        it('should respond with a 401 status code.', function(done) {
+          request(invalidPasswordOptions, function(err, res, body) {
+            expect(res.statusCode).to.equal(401);
+            done();
+          });
         });
+
+        it('should return a JSON object with an "error" property with a value of "Incorrect password."', function(done) {
+          request(invalidPasswordOptions, function(err, res, body) {
+            expect(body).to.eql(INVALID_PASSWORD_RESPONSE);
+            done();
+          });
+        });
+
       });
 
-      it('should return a JSON object with an "error" property with a value of "Incorrect password."', function(done) {
-        request(invalidPasswordOptions, function(err, res, body) {
-          expect(body).to.eql(INVALID_PASSWORD_RESPONSE);
-          done();
-        });
-      });
     });
 
     describe('with an invalid email', function() {
       var invalidEmailOptions = {
         url: LOGIN_URL,
-        method: 'POST',
-        json: INVALID_EMAIL
+        method: 'POST'
       };
 
-      it('should respond with a 401 status code.', function(done) {
-        request(invalidEmailOptions, function(err, res, body) {
-          expect(res.statusCode).to.equal(401);
-          done();
-        });
-      });
+      INVALID_EMAILS.forEach(function(credentials) {
+        invalidEmailOptions.json = credentials;
 
-      it('should return a JSON object with an "error" property with a value of "Email not found."', function(done) {
-        request(invalidEmailOptions, function(err, res, body) {
-          expect(body).to.eql(INVALID_EMAIL_RESPONSE);
-          done();
+        it('should respond with a 401 status code.', function(done) {
+          request(invalidEmailOptions, function(err, res, body) {
+            expect(res.statusCode).to.equal(401);
+            done();
+          });
         });
+
+        it('should return a JSON object with an "error" property with a value of "Email not found."', function(done) {
+          request(invalidEmailOptions, function(err, res, body) {
+            expect(body).to.eql(INVALID_EMAIL_RESPONSE);
+            done();
+          });
+        });
+
       });
 
     });
